e98e812adf45356fd12f1de39623b6ebd5a106c1
obassler <ondrabassler.o@gmail.com>
2025-08-21 18:05:19 +0200

    Initial commit

---
+++ src/main/java/com/example/talesofpango/TalesOfPangoApplication.java
@@ -0,0 +1,13 @@
+package com.example.talesofpango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalesOfPangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalesOfPangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/talesofpango/TalesOfPangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.talesofpango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalesOfPangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
